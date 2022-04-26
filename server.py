df1d1f4f113dc80fc52becc2f0b66096de91b173
David Fernandes <david.fernandes@pdmfc.com>
2022-04-26 19:14:42 +0300

    enabled server

--- server.py
+++ server.py
@@ -20,42 +20,36 @@ class MarketModule(MarketModuleServicer):
 
     def RunLongTermMarket(self, request: MarketInput, context) -> MarketOutput:
         input_dict = jsonpickle.decode(request.input)
+        output = run_longterm_market(input_dict)
         return MarketOutput(
-            Gn=jsonpickle.encode([], unpicklable=True)
+            Gn=jsonpickle.encode(output['Gn'], unpicklable=True),
+            Ln=jsonpickle.encode(output['Ln'], unpicklable=True),
+            Pn=jsonpickle.encode(output['Pn'], unpicklable=True),
+            QoE=jsonpickle.encode(output['QoE'], unpicklable=True),
+            optimal=output['optimal'],
+            plot_market_clearing=jsonpickle.encode(output['plot_market_clearing'], unpicklable=True),
+            settlement=jsonpickle.encode(output['settlement'], unpicklable=True),
+            social_welfare_h=jsonpickle.encode(output['social_welfare_h'], unpicklable=True),
+            shadow_price=jsonpickle.encode(output['shadow_price'], unpicklable=True),
+            Tnm=jsonpickle.encode(output['Tnm'], unpicklable=True),
+            agent_operational_cost=jsonpickle.encode(output['agent_operational_cost'], unpicklable=True),
         )
-        # output = run_longterm_market(input_dict)
-        # return MarketOutput(
-        #     Gn=jsonpickle.encode(output['Gn'], unpicklable=True),
-        #     Ln=jsonpickle.encode(output['Ln'], unpicklable=True),
-        #     Pn=jsonpickle.encode(output['Pn'], unpicklable=True),
-        #     QoE=jsonpickle.encode(output['QoE'], unpicklable=True),
-        #     optimal=output['optimal'],
-        #     plot_market_clearing=jsonpickle.encode(output['plot_market_clearing'], unpicklable=True),
-        #     settlement=jsonpickle.encode(output['settlement'], unpicklable=True),
-        #     social_welfare_h=jsonpickle.encode(output['social_welfare_h'], unpicklable=True),
-        #     shadow_price=jsonpickle.encode(output['shadow_price'], unpicklable=True),
-        #     Tnm=jsonpickle.encode(output['Tnm'], unpicklable=True),
-        #     agent_operational_cost=jsonpickle.encode(output['agent_operational_cost'], unpicklable=True),
-        # )
 
     def RunShortTermMarket(self, request: MarketInput, context) -> MarketOutput:
         input_dict = jsonpickle.decode(request.input)
-        # output = run_shortterm_market(input_dict)
-        # return MarketOutput(
-        #     Gn=jsonpickle.encode(output['Gn'], unpicklable=True),
-        #     Ln=jsonpickle.encode(output['Ln'], unpicklable=True),
-        #     Pn=jsonpickle.encode(output['Pn'], unpicklable=True),
-        #     QoE=jsonpickle.encode(output['QoE'], unpicklable=True),
-        #     optimal=output['optimal'],
-        #     plot_market_clearing=jsonpickle.encode(output['plot_market_clearing'], unpicklable=True),
-        #     settlement=jsonpickle.encode(output['settlement'], unpicklable=True),
-        #     social_welfare_h=jsonpickle.encode(output['social_welfare_h'], unpicklable=True),
-        #     shadow_price=jsonpickle.encode(output['shadow_price'], unpicklable=True),
-        #     Tnm=jsonpickle.encode(output['Tnm'], unpicklable=True),
-        #     agent_operational_cost=jsonpickle.encode(output['agent_operational_cost'], unpicklable=True),
-        # )
+        output = run_shortterm_market(input_dict)
         return MarketOutput(
-            Gn=jsonpickle.encode([], unpicklable=True)
+            Gn=jsonpickle.encode(output['Gn'], unpicklable=True),
+            Ln=jsonpickle.encode(output['Ln'], unpicklable=True),
+            Pn=jsonpickle.encode(output['Pn'], unpicklable=True),
+            QoE=jsonpickle.encode(output['QoE'], unpicklable=True),
+            optimal=output['optimal'],
+            plot_market_clearing=jsonpickle.encode(output['plot_market_clearing'], unpicklable=True),
+            settlement=jsonpickle.encode(output['settlement'], unpicklable=True),
+            social_welfare_h=jsonpickle.encode(output['social_welfare_h'], unpicklable=True),
+            shadow_price=jsonpickle.encode(output['shadow_price'], unpicklable=True),
+            Tnm=jsonpickle.encode(output['Tnm'], unpicklable=True),
+            agent_operational_cost=jsonpickle.encode(output['agent_operational_cost'], unpicklable=True),
         )
 
 
